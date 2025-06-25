6e99ffc60b7d74e755c929096ce313cfa306cf4a
fabjan1027 <fabjan1027@gmail.com>
2025-06-25 21:20:01 +0200

    feat: colorscheme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
 	{
 		"sainnhe/gruvbox-material",
-		priority = 1000,
 		lazy = false,
-		opts = {},
+		priority = 1000,
 		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
